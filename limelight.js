c7183ad10c35435ac7be14bebe91f9a7e34227df
Rofer11607 <rafac@monsterrg.com>
2022-02-04 12:38:55 -0500

    more rollbacks

--- limelight.js
+++ limelight.js
@@ -1,22 +1,24 @@
 const { execSync } = require("child_process");
 
-function main() {
-    executeLimelight()
+async function main() {
+    await executeLimelight();
 }
 
 async function executeLimelight() {
     execSync(`killall limelight &> /dev/null
-limelight &> /dev/null &`)
-    await sleep(5000)
+limelight &> /dev/null &`);
+    execSync("pkill limelight");
+    await sleep(1000);
     try {
         execSync("limelight");
-    } catch(err) {
-        main();
+    } catch (err) {
+        await main();
     }
+    return
 }
 
 function sleep(ms) {
-    return new Promise( (r)=> setTimeout(r,ms))
+    return new Promise((r) => setTimeout(r, ms));
 }
 
 main();
